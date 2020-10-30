4694736cc0f0c77e95a757eb24b079fe9bf158e7
yuke brilliant <yukebrilliant@gmail.com>
2020-10-30 12:46:14 +0700

    Login Register

--- public/js/app.js
+++ public/js/app.js
@@ -37339,8 +37339,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\e-vote\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\e-vote\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\e-voting\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\e-voting\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
